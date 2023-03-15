760d6f93bc7cfa2164f9ee4d1b45e686c4478457
avaneepk <119371288+avaneepk@users.noreply.github.com>
2023-03-15 19:48:34 +0200

    hospital tables

---
+++ python hospital project/hospital_doctor.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: hospital
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `doctor`
+--
+
+DROP TABLE IF EXISTS `doctor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `doctor` (
+  `Doctor_Name` varchar(50) DEFAULT NULL,
+  `age` int DEFAULT NULL,
+  `speciality` varchar(100) DEFAULT NULL,
+  `years_in_service` int DEFAULT NULL,
+  `qualification` varchar(100) DEFAULT NULL,
+  `institute` varchar(100) DEFAULT NULL,
+  `contact_no` int NOT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `workplace_name` varchar(100) DEFAULT NULL,
+  `workplace_no` int DEFAULT NULL,
+  `workplace_address` varchar(150) DEFAULT NULL,
+  PRIMARY KEY (`contact_no`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `doctor`
+--
+
+LOCK TABLES `doctor` WRITE;
+/*!40000 ALTER TABLE `doctor` DISABLE KEYS */;
+INSERT INTO `doctor` VALUES ('Aman',37,'radiologist',6,'mbbs','Royal Medical School',88976,'aman123@yahoo.com','Paras hospital',82342,'Sector-51'),('Yash',45,'cardiologist',14,'mbbs','AIIMS',98765,'yashkamal10@gmail.com','Medanta hospital',95674,'sector-47'),('Raj',42,'oncologist',12,'mbbs','AIIMS',99887,'raj_jr@yahoo.com','Medanta hospital',95683,'sector-47');
+/*!40000 ALTER TABLE `doctor` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-03-15 17:33:46
